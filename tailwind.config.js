07d51be442019a09456e06e00532deb650bb706e
Alvin Wijaya <alvinwijaya1012@gmail.com>
2024-07-31 21:16:38 +0700

    feat: add User Profile Personalization 1, 2, 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,25 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        bg: '#FFF8E1',
         primary: '#EC8F5E',
         secondary: '#F3B764',
+        'dark-grey': '#969696',
       },
       fontFamily: {
         'helvetica': ['Helvetica'],
         'helvetica-bold': ['Helvetica-Bold'],
         'helvetica-light': ['Helvetica-Light'],
       },
+      fontSize: {
+        'xs': '8px',
+        'sm': '12px',
+        'base': '14px',
+        'md': '16px',
+        'lg': '20px',
+        'xl': '24px',
+        '2xl': '32px',
+      }
     },
   },
   plugins: [],
